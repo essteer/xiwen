d93e48993fac980f6cd5d00fab7d695176d95aaf
Elliott <117514332+essteer@users.noreply.github.com>
2023-11-27 11:48:35 +0800

    add filesave util and code

--- src/prep.py
+++ src/prep.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from tqdm import tqdm
+from utils.save_data import save_csv
 
 ##########################################################################
 # Prepare files
 ##########################################################################
 
 ENCODING = "utf-8"
-FILEPATH = "./data/HSK 2013 Pleco.txt"
+DATA_IN = "./data/input/"
+DATA_OUT = "./data/output/"
+HSK_PATH = DATA_IN + "HSK 2013 Pleco.txt"
+FREQ_PATH = DATA_IN + "CharFreq-Modern-utf8.csv"
 
 ##########################################################################
 # Initialise variables
@@ -18,10 +22,10 @@ vocab_dict = {}
 simp_chars, trad_chars = set(), set()
 
 ##########################################################################
-# Extract data
+# Extract HSK dataset to DataFrame
 ##########################################################################
 
-with open(FILEPATH, "r", encoding=ENCODING) as f:
+with open(HSK_PATH, "r", encoding=ENCODING) as f:
     lines = f.readlines()
     for line in tqdm(lines, desc="Processing"):
         
@@ -50,15 +54,42 @@ with open(FILEPATH, "r", encoding=ENCODING) as f:
             # Skip blank lines
             continue
 
+
+cols = ["Simplified", "Traditional", "Pinyin", "HSK Grade"]
+df = pd.DataFrame.from_dict(vocab_dict, orient="index", columns=cols)
+
 ##########################################################################
-# Extract data
+# Extract Jun Da MTSU character frequencies
 ##########################################################################
 
-columns = ["Simplified", "Traditional", "Pinyin", "HSK Grade"]
-df = pd.DataFrame.from_dict(vocab_dict, orient="index", columns=columns)
+junda_freqs = []
 
-df.head()
-
+with open(FREQ_PATH, "r", encoding=ENCODING) as f:
+    lines = f.readlines()
+    for line in tqdm(lines[6:], desc="Processing"):
         
+        data = line.split(",")
+        
+        codepoint = ord(data[1])
+        
+        junda_freqs.append([data[1], int(data[0]), int(data[2]), float(data[3]), codepoint])
         
 
+cols = ["Simplified", "JD Rank (2004)", "JD Frequency", "JD Percentile", "Unicode"]
+junda_df = pd.DataFrame(junda_freqs, columns=cols)        
+
+##########################################################################
+# Map frequencies to HSK set
+##########################################################################
+
+hskhanzi_df = df.merge(junda_df[junda_df["Simplified"].isin(df["Simplified"])], on="Simplified")
+
+##########################################################################
+# Save files
+##########################################################################
+
+# save_csv(df,          DATA_OUT, "hsk_vocab",         ENCODING)
+# save_csv(junda_df,    DATA_OUT, "junda_frequencies", ENCODING)
+# save_csv(hskhanzi_df, DATA_OUT, "hsk_hanzi",         ENCODING)
+# save_csv(simp_chars,  DATA_OUT, "hsk_simp_chars",    ENCODING)
+# save_csv(trad_chars,  DATA_OUT, "hsk_trad_chars",    ENCODING)
---
+++ src/utils/save_data.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+import csv
+import pandas as pd
+
+def save_csv(data: pd.DataFrame | list | set, path: str, name: str, enc: str, new="\n") -> str:
+    """
+    Saves Pandas DataFrames and Python iterables
+    Args:
+        data - the content to be saved
+        path - str, location of intended directory
+        name - str, filename
+        enc - str, encoding (e.g. utf-8)
+        new - str, newlines (e.g. \n)
+    """
+    filename = name + ".csv"
+    filepath = os.path.join(path, filename)
+    
+    try:
+        if isinstance(data, pd.DataFrame):
+            data.to_csv(filepath, encoding=enc)
+        
+        else:
+            with open(filepath, "w", encoding=enc, newline=new) as f:
+                writer = csv.writer(f, delimiter=",")
+                for element in data:
+                    writer.writerow(str(element))
+                
+        return f"Saved {name}.csv with {enc} encoding"
+    
+    except Exception as e:
+        return f"Save failed for {name}: {str(e)}"
+
