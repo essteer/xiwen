7131bbf8d58edff24d52674ca22251f0163f1012
Elliott <117514332+essteer@users.noreply.github.com>
2023-11-28 17:17:22 +0800

    create helper functions for character counts

--- src/interface.py
+++ src/interface.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from tqdm import tqdm
-from utils.hanzi_funcs import filter_text, partition_hanzi, identify
+from utils.hanzi_funcs import filter_text, partition_hanzi, identify, get_counts
 from utils.save_data import save_csv
 
 ##########################################################################
@@ -15,6 +15,7 @@ DATA_OUT = "./data/output/"
 HSK_HANZI = pd.read_csv(DATA_OUT + "hsk_hanzi.csv")
 HSK_SIMP = list(HSK_HANZI["Simplified"])
 HSK_TRAD = list(HSK_HANZI["Traditional"])
+HSK_LEVELS = range(1, 7)
 # Test case (simplified characters)
 BEIJINGZHE = DATA_IN + "beijingzhe.txt"
 
@@ -34,12 +35,21 @@ simp, trad, neutral, outliers = partition_hanzi(HSK_SIMP, HSK_TRAD, hanzi_list)
 # Analyse content
 ##########################################################################
 
-num_hanzi = len(hanzi_list)
-num_unique = len(set(hanzi_list))
 # Query character variant
 variant = identify(simp, trad, neutral)
+variants = {
+    "Simplified": simp, 
+    "Traditional": trad, 
+    "Unknown": (simp, trad)
+}
 
-print(f"=== Variant: {variant.title()} Chinese ===")
+hanzi_df = HSK_HANZI.copy()
+stats = get_counts(hanzi_df, variants[variant], variant)
+
+num_hanzi  = len(hanzi_list)
+num_unique = len(set(hanzi_list))
+
+print(f"=== {variant} character set ===")
 
 print(f"{num_hanzi:,} Chinese character{'s' if num_hanzi != 1 else ''} found,\nof which:")
 print(f"-> {len(simp):,} ({len(simp)/num_hanzi:.2%}) ∈ [HSK1, HSK6]")
@@ -48,7 +58,10 @@ print(f"-> {len(neutral):,} ({len(neutral)/num_hanzi:.2%}) common to simp. and t
 print(f"{num_unique:,} unique characters,\nof which:")
 unique_simp = set(simp)
 print(f"-> {len(unique_simp):,} ({len(unique_simp)/num_unique:.2%}) ∈ [HSK1, HSK6]")
-unique_others = set(outliers)
+unique_outliers = set(outliers)
 
-print(f"-> {num_unique - len(unique_simp):,} ({len(unique_others)/num_unique:.2%}) > HSK6")
+print(f"-> {num_unique - len(unique_simp):,} ({len(unique_outliers)/num_unique:.2%}) > HSK6")
+
+hsk_grade = HSK_HANZI[HSK_HANZI["Simplified"] == "爸"]["HSK Grade"].values[0]
+
 
--- src/utils/hanzi_funcs.py
+++ src/utils/hanzi_funcs.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
 
-def filter_hanzi(char: str) -> bool:
+def _filter_hanzi(char: str) -> bool:
     """
     Checks whether a character is a (possible)
     Chinese character against three Unicode sets:
@@ -27,10 +28,10 @@ def filter_hanzi(char: str) -> bool:
 
 def filter_text(text):
     """
-    Passes text to filter_hanzi
+    Passes text to _filter_hanzi
     Returns list of hanzi in text
     """
-    return [zi for zi in text if filter_hanzi(zi)]
+    return [zi for zi in text if _filter_hanzi(zi)]
 
 
 def partition_hanzi(hsk_simp: list, hsk_trad: list, hanzi_list: list) -> tuple[list]:
@@ -57,7 +58,8 @@ def partition_hanzi(hsk_simp: list, hsk_trad: list, hanzi_list: list) -> tuple[l
 
 def identify(hsk_simp: list, hsk_trad: list, neutral: list) -> tuple[str, float]:
     """
-    Identifies text as either simp, trad, or undefined
+    Identifies text as either simplified or traditional Chinese, 
+        or unknown if the variant is uncertain
     Args:
         - hsk_simp, list, simplified characters in HSK1 to HSK6
         - hsk_trad, list, traditional equivalents to hsk_simp
@@ -72,13 +74,114 @@ def identify(hsk_simp: list, hsk_trad: list, neutral: list) -> tuple[str, float]
     try:
         ratio = len(simp_set) / (len(simp_set) + len(trad_set))
         if ratio >= threshold:
-            return "simplified"
+            return "Simplified"
         elif ratio <= 1 - threshold:
-            return "traditional"
+            return "Traditional"
         else:
-            return "undefined"
+            return "Unknown"
         
     except ZeroDivisionError:
-        return "undefined"
+        return "Unknown"
+
+
+def _counts(hanzi: list) -> dict:
+    """
+    Counts occurrences of each character in hanzi
+    Args:
+        - hanzi, list, characters to count
+    Returns:
+        - counts, dict, counts of each character
+    """
+    counts = {}
+    for zi in hanzi:
+            counts[zi] = counts.get(zi, 0) + 1
+    return counts
+
+
+def _granular_counts(df: pd.DataFrame, variant: str) -> list:
+    """
+    Breaks down counts according to HSK grades
+    Args:
+        - df, Pandas DataFrame, all unique characters in HSK1 to HSK6
+            with counts column
+        - variant, str, the variant of the character set (Simplified|Traditional|Unknown)
+    Returns:
+        - dict, counts for characters per HSK grade
+    """
+    # TODO: add code for count breakdowns
+    pass
+
+
+def get_counts(df: pd.DataFrame, hanzi: list|tuple[list], variant: str):
+    """
+    Passes hanzi to _counts to count occurrences of each Chinese character
+    Passes updated df to _granular_counts for grade-by-grade breakdown
     
+    Args:
+        - df, Pandas DataFrame, all unique characters in HSK1 to HSK6
+        - hanzi, list | tuple, the characters to be analysed
+            list if variant defined as either simplified or traditional
+            tuple of simplified and traditional lists if unknown
+        - variant, str, the variant of the character set (Simplified|Traditional|Unknown)
+    Returns:
+        - dict, stats for aggregate and grade-based character counts
+    """
+    # hanzi = single list if variant defined (Simplified|Traditional)
+    if isinstance(hanzi, list):
+        # Count occurrences of each character in hanzi
+        counts = _counts(hanzi)
+        # Create DataFrame from counts dictionary
+        counts_df = pd.DataFrame(list(counts.items()), columns=[variant, "Count"])
+        # Merge on variant column
+        merged_df = pd.merge(df, counts_df, on=variant, how="left")
+    
+    # hanzi = tuple of (Simplified, Traditional) characters if variant unknown
+    else:        
+        simp = hanzi[0]
+        # Avoid double-counting characters common to both variants
+        trad = [zi for zi in hanzi[1] if zi not in simp]
+        # Count occurrences of each character
+        s_counts = _counts(simp)
+        t_counts = _counts(trad)
+        
+        # Count both character variants for undefined cases
+        simp_counts_df = pd.DataFrame(list(s_counts.items()), columns=["Simplified", "Count"])
+        trad_counts_df = pd.DataFrame(list(t_counts.items()), columns=["Traditional", "Count"])
+        
+        # Merge variant counts separately
+        merged_df = pd.merge(df, simp_counts_df, on="Simplified", how="left")
+        merged_df = pd.merge(merged_df, trad_counts_df, on="Traditional", how="left")
+        
+        # Create mask for rows with identical variants
+        identical_mask = merged_df["Simplified"] == merged_df["Traditional"]
+        
+        # Sum counts for rows with different variants
+        merged_df.loc[~identical_mask, "Count"] = (
+            merged_df["Count_x"].fillna(0) + merged_df["Count_y"].fillna(0)
+        )
+        # Take either count for rows with identical variants
+        merged_df.loc[identical_mask, "Count"] = merged_df["Count_x"].fillna(0)
+        
+        # Drop unnecessary columns
+        merged_df.drop(columns=["Count_x", "Count_y"], inplace=True)
+    
+    # Fill NaN as 0 and convert counts to integers
+    merged_df["Count"] = merged_df["Count"].fillna(0).astype(int)
+    
+    # TODO: pass merged_df to _granular_counts() for breakdowns
+    
+    return merged_df
+    
+    
+##########################################################################
+# Unit test for get_counts()
+##########################################################################
+
+# df_data = {"Simplified": ["爱", "八", "爸", "杯", "子"], "Traditional": ["愛", "八", "爸", "杯", "子"]}
+# hanzi_simplified_data = ["爱", "八", "爸", "杯", "子"]
+# hanzi_traditional_data = ["愛", "八", "爸", "杯", "子"]
+
+# df = pd.DataFrame(df_data)
+# result_df = get_counts(df, (hanzi_simplified_data, hanzi_traditional_data), "Undefined")
+# print(result_df)
     
