183c6b5ee49321c060faf3b8ccf300795686695c
essteer <essteer@pm.me>
2024-09-07 18:56:13 +0800

    refactor: Expand docstring return object description, remove redundant commented code and reduce interval between request attempts

--- src/xiwen/utils/html.py
+++ src/xiwen/utils/html.py
@@ -15,20 +15,19 @@ def get_html(url: str) -> BeautifulSoup:
 
     Returns
     -------
-    HTML extracted from URL
+    _ : BeautifulSoup
+        HTML extracted from URL
     """
-    number_of_retries = 3
-    for i in range(number_of_retries):
+    max_retries = 3
+    for i in range(max_retries):
         try:
-            # Get randomised user-agent and referer data
-            header = masq(ua=True, rf=True)
+            header = masq(
+                ua=True, rf=True
+            )  # Get weighted-random user-agent and referer
             header["Accept-Language"] = "en-US,en;q=0.9;q=0.7,zh-CN;q=0.6,zh;q=0.5"
-            # Get URL content
             response = requests.get(url, headers=header, timeout=10)
-            # Catch errors
             response.raise_for_status()
-            # Extract and parse HTML
             return BeautifulSoup(response.text, "html.parser")
 
         except requests.exceptions.RequestException:
-            time.sleep(2 ** (i + 1))
+            time.sleep(2**i)
